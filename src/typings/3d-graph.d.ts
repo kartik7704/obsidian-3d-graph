d702df388056a9aba6a7ea4fe3e554998a9aa8b6
kk <kartik7704@gmail.com>
2026-06-23 22:50:28 +0530

    fix: add d3-force-3d module declaration to 3d-graph.d.ts

--- src/typings/3d-graph.d.ts
+++ src/typings/3d-graph.d.ts
@@ -6,3 +6,15 @@ type Coords = {
   y: number;
   z: number;
 };
+
+declare module "d3-force-3d" {
+  export function forceSimulation<N>(nodes?: N[]): any;
+  export function forceLink<N, L>(links?: L[]): any;
+  export function forceManyBody(): any;
+  export function forceCenter(x?: number, y?: number, z?: number): any;
+  export function forceCollide(radius?: number | ((node: any) => number)): any;
+  export function forceX(x?: number | ((node: any) => number)): any;
+  export function forceY(y?: number | ((node: any) => number)): any;
+  export function forceZ(z?: number | ((node: any) => number)): any;
+  export function forceRadial(radius: number | ((node: any) => number), x?: number, y?: number, z?: number): any;
+}
